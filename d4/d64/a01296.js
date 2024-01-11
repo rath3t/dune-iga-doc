b83ccbc9e8936c48d7be21e6964045ceaac3cf64
rath3t <rath3t@users.noreply.github.com>
2024-01-11 17:39:36 +0000

    deploy: rath3t/dune-iga@f77e5c91008ef2454ed3edb3e241d31d3141b1a5

--- d4/d64/a01296.js
+++ d4/d64/a01296.js
@@ -1,5 +1,4 @@
 var a01296 =
 [
-    [ "read", "d4/d64/a01296.html#a7751120f5ee287eaed14728396bd01d7", null ],
-    [ "read", "d4/d64/a01296.html#aaa1ccaadc5a461f1d09e6c2f6e401d4f", null ]
+    [ "read", "d4/d64/a01296.html#a7751120f5ee287eaed14728396bd01d7", null ]
 ];
--- dd/d32/a00041.js
+++ dd/d32/a00041.js
@@ -1,4 +1,5 @@
 var a00041 =
 [
-    [ "readJson", "dd/d32/a00041.html#a98a9abffb5b3f36b7428f27ba9ec4292", null ]
+    [ "readJson", "dd/d32/a00041.html#a54b6cf582685f1241781602bb2a0e735", null ],
+    [ "readJson", "dd/d32/a00041.html#abe48967cf956f798eea3f80cc1edc59e", null ]
 ];
