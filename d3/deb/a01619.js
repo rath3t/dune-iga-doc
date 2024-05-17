d3fbbb53ace7800964277fcf9d3f26e422216a06
henrij22 <henrij22@users.noreply.github.com>
2024-05-17 13:04:22 +0000

    deploy: rath3t/dune-iga@908398d6f129a9288509c6d251b33e1e4a5fc305

--- d3/deb/a01619.js
+++ d3/deb/a01619.js
@@ -5,10 +5,10 @@ var a01619 =
     [ "boundaryDivisions", "d3/deb/a01619.html#a8624608edbeb6affadb1e1129ffc9a70", null ],
     [ "getInstance", "d3/deb/a01619.html#a19c3b252a6daa55f42f41a6d9e03e173", null ],
     [ "operator=", "d3/deb/a01619.html#ae49006fbc7885b55759167099bfa14ea", null ],
+    [ "reconstructTrimmedLocalGeometry", "d3/deb/a01619.html#adf5f44c1e76a6121090510c8fbdab6ad", null ],
+    [ "reconstructTrimmedLocalGeometry", "d3/deb/a01619.html#a18a9676b02aa52fc2e01208d36279f6d", null ],
     [ "reportTrimmedElementGeometryTypeAsNone", "d3/deb/a01619.html#a9ec6d02568a5e28528445e58bc361b2e", null ],
     [ "reportTrimmedElementGeometryTypeAsNone", "d3/deb/a01619.html#ae57af2c61e14419a95d49f6f75a8e0f9", null ],
-    [ "reportTrimmedIntersections", "d3/deb/a01619.html#a1d8b230262ab321e15eb7c312ed649cc", null ],
-    [ "reportTrimmedIntersections", "d3/deb/a01619.html#a9ec73f69fa5ca258e916655a1b6c0f8a", null ],
     [ "targetAccuracy", "d3/deb/a01619.html#a0ed4096cdd5500429567dc33c2a59ba0", null ],
     [ "targetAccuracy", "d3/deb/a01619.html#a9c1e29d25ce10f49e9bd4c125d95f9ea", null ]
 ];
